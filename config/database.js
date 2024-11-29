febcc1a90fbce64233a35c7420a8146504c07b7e
joshuanelsondev <joshuanelsondev@gmail.com>
2024-11-28 23:58:32 -0500

    refactor: update server and database

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+const config = require('./config');
+
+const env = process.env.NODE_ENV || 'development';
+console.log('config[env]: ', config);
+const { url, dialect } = config[env];
+
+const sequelize = new Sequelize(url, {
+  dialect: dialect || 'postgres',
+  logging: false,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const app = require('./app')
+const express = require("express");
+const sequelize = require("./config/database");
+const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`[server] Server is running at http://localhost:${PORT}`)
-})
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("All models were synchronized successfully.");
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`[server] Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error synchronizing models: ", error);
+  });
+
+module.exports = app;
