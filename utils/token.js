eef0469a46cde68bed52da9910f6f070327548c7
joshuanelsondev <joshuanelsondev@gmail.com>
2024-11-30 10:03:31 -0500

    refactor: rename generateToken to token

---
+++ utils/token.js
@@ -0,0 +1,8 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+
+const generateToken = (payload, expiresIn = "1h") => {
+  return jwt.sign(payload, config.JWT_SECRET, { expiresIn });
+};
+
+module.exports = generateToken;
