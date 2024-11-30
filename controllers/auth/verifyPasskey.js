dfc3e230c0df88a5507c4ad9a4aab878c1ad61e6
joshuanelsondev <joshuanelsondev@gmail.com>
2024-11-30 10:58:22 -0500

    feat: create verifyPasskey controller file

---
+++ controllers/auth/verifyPasskey.js
@@ -0,0 +1,43 @@
+const { server } = require("@passwordless-id/webauthn");
+const db = require("../../models");
+const config = require("../../config");
+const normalizeChallenge = require("../../utils/challengeUtils");
+const generateToken = require("../../utils/token");
+
+const verifyPasskey = async (req, res) => {
+  try {
+    const { credential, email } = req.body;
+    const user = await db.User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Format expected challenge
+    const normalizedChallenge = normalizeChallenge(user.challenge);
+
+    const verification = await server.verifyAuthenticationResponse({
+      response: credential,
+      normalizedChallenge,
+      expectedOrigin: config.EXPECTED_ORIGIN,
+      expectedRPID: config.EXPECTED_RPID,
+      userVerification: "required",
+      counter: user.authCounter,
+    });
+
+    if (verification.verified) {
+      user.authCounter = verification.authenticationInfo.counter;
+      await user.save();
+
+      const token = generateToken({ id: user.id });
+
+      res.status(200).json({ message: "Login successful", token });
+    } else {
+      res.status(400).json({ message: "Invalid authentication response" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error verifying passkey", error });
+  }
+};
+
+module.exports = verifyPasskey;
