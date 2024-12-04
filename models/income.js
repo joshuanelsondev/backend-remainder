8033da7c853945995385b8770597042d2772b767
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-03 22:53:16 -0500

    feat: add associations for models

--- models/income.js
+++ models/income.js
@@ -2,13 +2,11 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Income extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Income.belongsTo(models.User, {
+        foreignKey: "userId",
+        as: "users",
+      });
     }
   }
   Income.init(
