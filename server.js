c7e92eaebffaf1d3ae7acaa803b8acaf92a5b1c8
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-05 02:47:17 -0500

    refactor: remove redundancy of sequelize instance in database config file

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const app = require('./app');
-const sequelize = require('./config/database');
+const app = require("./app");
+const sequelize = require("./config/database");
+const process = require("process");
 
 const PORT = process.env.PORT || 3000;
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
-    if (process.env.NODE_ENV !== 'production') {
+    console.log("Connection has been established successfully.");
+    if (process.env.NODE_ENV !== "production") {
       return sequelize.sync();
     }
   })
   .then(() => {
-    console.log('All models were synchronized successfully.');
+    console.log("All models were synchronized successfully.");
     app.listen(PORT, () => {
       console.log(`[server] Server is running at http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
     console.error(
-      'Error synchronizing models or connecting to the database:',
+      "Error synchronizing models or connecting to the database:",
       error.message,
       error.stack
     );
