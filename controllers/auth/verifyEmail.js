230e049049a8b6304d861c50cf170f1f618579ec
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-07 04:04:03 -0500

    refactor: update verifyEmail controller

--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -1,6 +1,6 @@
-const { verifyToken } = require("../../utils/token");
-const process = require("process");
-const db = require("../../models");
+const { verifyToken } = require('../../utils/token');
+const process = require('process');
+const db = require('../../models');
 
 const verifyEmailController = async (req, res) => {
   const { token } = req.params;
@@ -11,15 +11,16 @@ const verifyEmailController = async (req, res) => {
     const user = await db.User.findOne({ where: { id: decoded.id } });
 
     if (!user) {
-      return res.status(404).send("User not found");
+      return res.status(404).send('User not found');
     }
 
     user.isVerified = true;
+    user.verificationToken = null;
     await user.save();
 
-    res.redirect(`${process.env.FRONTEND_URL}/verification-success`);
+    res.redirect(`${process.env.FRONTEND_URL}/mfa-setup?email=${user.email}`);
   } catch (error) {
-    console.error(error);
+    console.error('Error verifying email:', error);
     res.redirect(`${process.env.FRONTEND_URL}/verification-failed`);
   }
 };
