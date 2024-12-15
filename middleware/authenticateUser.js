4ea72737f19b22d1e1627eb2f93a513393385a80
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-15 16:21:48 -0500

    fix: update import bugs

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -30,12 +30,4 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-const authorizeAdmin = (req, res, next) => {
-  if (req.user.isAdmin) {
-    return next();
-  }
-
-  return res.status(403).json({ message: "Admin privileges required" });
-};
-
 module.exports = authenticateUser;
